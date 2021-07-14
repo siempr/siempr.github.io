11b46e6ce21cc9a96c00b42ccb17c33d97f47c5a
siempr <87255975+siempr@users.noreply.github.com>
2021-07-14 22:10:58 +0800

    1

---
+++ mhlizc.github.io-main/scripts/playlist2.js
@@ -0,0 +1,12 @@
+﻿trackList = [
+	{
+		name:"P!nk - Raise Your Glass",
+		artist:"P!nk",
+		src:"music/y2mate.com - Pnk  Raise Your Glass.mp3"
+	},
+	{
+		name:"Bon Jovi - It's My Life",
+		artist:"Bon Jovi",
+		src:"music/y2mate.com - Bon Jovi  Its My Life Official Music Video.mp3"
+	},
+];
---
+++ mhlizc.github.io-main/scripts/playlisteng.js
@@ -0,0 +1,222 @@
+trackList = [
+	{
+		name:"Ed Sheeran - Castle On The Hill [Official Lyric Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Castle On The Hill [Official Lyric Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Galway Girl [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Galway Girl [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Happier (Official Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Happier (Official Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - I See Fire (Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - I See Fire (Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Perfect (Official Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Perfect (Official Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Photograph (Official Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Photograph (Official Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Shape of You [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Shape of You [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - The A Team [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - The A Team [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Thinking Out Loud [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Thinking Out Loud [Official Video].mp3"
+	},
+	{
+		name:"Lukas Graham - 7 Years [OFFICIAL MUSIC VIDEO]",
+		artist:"Lukas Graham",
+		src:"eng_music/Lukas Graham - 7 Years [OFFICIAL MUSIC VIDEO].mp3"
+	},
+	{
+		name:"Lukas Graham - Love Someone [OFFICIAL MUSIC VIDEO]",
+		artist:"Lukas Graham",
+		src:"eng_music/Lukas Graham - Love Someone [OFFICIAL MUSIC VIDEO].mp3"
+	},
+	{
+		name:"What Do I Know - Ed Sheeran [Lyric]",
+		artist:"Ed Sheeran",
+		src:"eng_music/What Do I Know - Ed Sheeran [Lyric].mp3"
+	},
+	{
+		name:"Tyla Yaweh Tommy Lee - Tommy Lee",
+		artist:"Tommy Lee",
+		src:"eng_music/Tyla Yaweh Tommy Lee - Tommy Lee.mp3"
+	},
+	{
+		name:"Multiple Artists - songs to self-isolate to",
+		artist:"TMultiple Artists",
+		src:"eng_music/Multiple Artists - songs to self-isolate to.mp3"
+	},
+	{
+		name:"XXXTENTACION Juice WRLD - where did u go (Ocean Mix)",
+		artist:"XXXTENTACION Juice WRLD",
+		src:"eng_music/XXXTENTACION Juice WRLD - where did u go (Ocean Mix).mp3"
+	},
+	{
+		name:"Post Malone - My Friends",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - My Friends.mp3"
+	},
+	{
+		name:"Lofi - Chill aesthetic music playlist",
+		artist:"Lofi",
+		src:"eng_music/Lofi - Chill aesthetic music playlist.mp3"
+	},
+	{
+		name:"Deja Vu- Post Malone ft. Justin Bieber (official audio)",
+		artist:"Deja Vu",
+		src:"eng_music/Deja Vu- Post Malone ft. Justin Bieber (official audio).mp3"
+	},
+	{
+		name:"Post Malone - A Thousand Bad Times (Audio)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - A Thousand Bad Times (Audio).mp3"
+	},
+	{
+		name:"Post Malone - Better Now",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Better Now.mp3"
+	},
+	{
+		name:"Post Malone - Candy Paint (The Fate of the Furious The Album) [Official Audio]",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Candy Paint (The Fate of the Furious The Album) [Official Audio].mp3"
+	},
+	{
+		name:"Post Malone - Circles",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Circles.mp3"
+	},
+	{
+		name:"Post Malone - Congratulations ft. Quavo",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Congratulations ft. Quavo.mp3"
+	},
+	{
+		name:"Post Malone - Die For Me (Audio) ft. Future, Halsey",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Die For Me (Audio) ft. Future, Halsey.mp3"
+	},
+	{
+		name:"Post Malone - Enemies feat. DaBaby (Lyrics)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Enemies feat. DaBaby (Lyrics).mp3"
+	},
+	{
+		name:"Post Malone - Go Flex",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Go Flex.mp3"
+	},
+	{
+		name:"Post Malone - Goodbyes ft. Young Thug",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Goodbyes ft. Young Thug.mp3"
+	},
+	{
+		name:"Post Malone - Im Gonna Be (Audio)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Im Gonna Be (Audio).mp3"
+	},
+	{
+		name:"Post Malone - Myself (Audio)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Myself (Audio).mp3"
+	},
+	{
+		name:"Post Malone - Paranoid (Lyrics)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Paranoid (Lyrics).mp3"
+	},
+	{
+		name:"Post Malone - Paranoid (Lyrics)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Paranoid (Lyrics).mp3"
+	},
+	{
+		name:"Post Malone - rockstar ft. 21 Savage",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - rockstar ft. 21 Savage.mp3"
+	},
+	{
+		name:"Post Malone - Saint-Tropez (Official Video)",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Saint-Tropez (Official Video).mp3"
+	},
+	{
+		name:"Post Malone - These Days",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - These Days.mp3"
+	},
+	{
+		name:"Post Malone - Too Young",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Too Young.mp3"
+	},
+	{
+		name:"Post Malone - White Iverson",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - White Iverson.mp3"
+	},
+	{
+		name:"Post Malone - Wow",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone - Wow.mp3"
+	},
+	{
+		name:"Post Malone, Swae Lee - Sunflower",
+		artist:"Post Malone",
+		src:"eng_music/Post Malone, Swae Lee - Sunflower.mp3"
+	},
+	{
+		name:"Rich and Sad",
+		artist:"",
+		src:"eng_music/Rich and Sad.mp3"
+	},
+	{
+		name:"Juice W R L D Tribute Mixtape",
+		artist:"",
+		src:"eng_music/Juice W R L D Tribute Mixtape.mp3"
+	},
+	{
+		name:"Marshmello Sanctuary Mix",
+		artist:"",
+		src:"eng_music/Marshmello Sanctuary Mix.mp3"
+	},
+	{
+		name:"Lil Revive x Darko - SLASHER",
+		artist:"Lil Revive x Darko",
+		src:"eng_music/Lil Revive x Darko - SLASHER.mp3"
+	},
+	{
+		name:"Josh A - DISGRACE",
+		artist:"Josh A ",
+		src:"eng_music/Josh A - DISGRACE.mp3"
+	},
+	{
+		name:"Lil Revive - GRIM PEAKS",
+		artist:"Lil Revive",
+		src:"eng_music/Lil Revive - GRIM PEAKS.mp3"
+	}
+];
